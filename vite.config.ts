137682c20c06a29a4e453bf8c0f939538a42ae5f
lixin <lixin@zhongbaounion.com>
2022-10-13 17:30:53 +0800

    出发部署GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ const API_DOMAIN = '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: './docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
