876055db9d3997b7677b8b39ee8451c39ae15d48
lixin <lixin@zhongbaounion.com>
2022-10-13 10:29:13 +0800

    修改eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-const API_DOMAIN = "";
+const API_DOMAIN = '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 8888,
     open: true,
     proxy: {
-      "/api": {
+      '/api': {
         target: API_DOMAIN,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
