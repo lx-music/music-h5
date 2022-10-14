0adc139bdeec7a3ff3b7c5f338420dc4978390eb
lixin <lixin@zhongbaounion.com>
2022-10-14 18:34:38 +0800

    nginx

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL || '/music-h5'),
   history: createWebHistory('/music-h5'),
   routes: [
     {
