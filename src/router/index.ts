be2237379795c786a5c4eecb72fbafed685de383
lixin <lixin@zhongbaounion.com>
2022-10-14 18:12:43 +0800

    根路由方式

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL || '/music-h5'),
+  history: createWebHistory('/music-h5'),
   routes: [
     {
       path: '/',
